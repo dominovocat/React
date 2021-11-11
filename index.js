0e4345282e481d7b0680048692d761fb11888613
danil <minakonn9shkoni@gmail.com>
2021-11-11 10:52:37 +0300

    Add correction to Counter

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-class Counter extends React.Component{
-  constructor(props){
+class Counter extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {counter:0};
+    this.state = { counter: 0 };
   }
-  render(){
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decrement = () => {
     const {counter} = this.state;
+    if (counter > 0) {
+      this.setState({ counter: counter - 1 });
+    }
+  };
+  render() {
+    const { counter } = this.state;
     return React.createElement(
       React.Fragment,
       null,
-      React.createElement('h1',null,counter),
-      React.createElement('button',null,'+'),
-      React.createElement('button',null,'-'),
-    )
+      React.createElement("h1", null, counter),
+      React.createElement("button", { onClick: this.increment }, "+"),
+      React.createElement("button", { onClick: this.decrement }, "-")
+    );
   }
 }
 const reactElement = React.createElement(Counter);
-ReactDOM.render(reactElement,document.getElementById('root'))
+ReactDOM.render(reactElement, document.getElementById("root"));
