5390fd96c22c04ea801867b7a9e8e61565384f0d
danil <minakonn9shkoni@gmail.com>
2021-11-11 09:45:45 +0300

    Add Counter

---
+++ index.js
@@ -0,0 +1,18 @@
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {counter:0};
+  }
+  render(){
+    const {counter} = this.state;
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement('h1',null,counter),
+      React.createElement('button',null,'+'),
+      React.createElement('button',null,'-'),
+    )
+  }
+}
+const reactElement = React.createElement(Counter);
+ReactDOM.render(reactElement,document.getElementById('root'))
